ef3714544ca0c8bd1c30e60fadd89accec38dfe0
bahaaelseisy-sudo <bahaaelseisy@gmail.com>
2026-01-31 16:04:53 +0300

    Add files via upload

--- Peering.tf
+++ Peering.tf
@@ -1,8 +1,8 @@
 resource "azurerm_virtual_network_peering" "from_spoke1-to-hub" {
     name = "from-spoke1-to-hub"
-    resource_group_name = azurerm_resource_group.rg1.name
-    virtual_network_name = azurerm_virtual_network.spoke1.name
-    remote_virtual_network_id = azurerm_virtual_network.hub.id
+    resource_group_name = azurerm_resource_group.Prod_rg
+    virtual_network_name = azurerm_virtual_network.spoke1-vnet
+    remote_virtual_network_id = azurerm_virtual_network.hub_vnet
     allow_forwarded_traffic = true
     allow_virtual_network_access = true
     allow_gateway_transit = false
@@ -11,9 +11,9 @@ resource "azurerm_virtual_network_peering" "from_spoke1-to-hub" {
 }
 resource "azurerm_virtual_network_peering" "from-hub-to-spoke1" {
     name = "from-hub-to-spoke1"
-    resource_group_name = azurerm_resource_group.rg1.name
-    virtual_network_name = azurerm_virtual_network.hub_vnet.name
-    remote_virtual_network_id = azurerm_virtual_network.spoke1-vnet.id
+    resource_group_name = azurerm_resource_group.hub_rg
+    virtual_network_name = azurerm_virtual_network.hub_vnet
+    remote_virtual_network_id = azurerm_virtual_network.spoke1-vnet
     allow_forwarded_traffic = true
     allow_virtual_network_access = true
     allow_gateway_transit = false
@@ -23,9 +23,9 @@ resource "azurerm_virtual_network_peering" "from-hub-to-spoke1" {
 
 resource "azurerm_virtual_network_peering" "From_hub_to_spoke2" { 
     name = "From_hub_to_spoke2"
-    resource_group_name = azurerm_resource_group.rg1.name
-    virtual_network_name = azurerm_virtual_network.hub_vnet.name
-    remote_virtual_network_id = azurerm_virtual_network.spoke2-vnet.id
+    resource_group_name = azurerm_resource_group.hub_rg
+    virtual_network_name = azurerm_virtual_network.hub_vnet
+    remote_virtual_network_id = azurerm_virtual_network.spoke2-vnet
     allow_forwarded_traffic = true
     allow_virtual_network_access = true
   allow_gateway_transit = false
@@ -33,9 +33,9 @@ resource "azurerm_virtual_network_peering" "From_hub_to_spoke2" {
 }
 resource "azurerm_virtual_network_peering" "from_spoke2_to_hub" {
     name = "from_spoke2_to_hub"
-    resource_group_name = azurerm_resource_group.rg1.name
-    virtual_network_name = azurerm_virtual_network.spoke2.name
-    remote_virtual_network_id = azurerm_virtual_network.hub.id
+    resource_group_name = azurerm_resource_group.Prod_rg
+    virtual_network_name = azurerm_virtual_network.spoke2-vnet
+    remote_virtual_network_id = azurerm_virtual_network.hub_vnet
     allow_forwarded_traffic = true
     allow_virtual_network_access = true
     allow_gateway_transit = false
