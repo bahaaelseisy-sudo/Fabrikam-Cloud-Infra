91a26a2f3430b3c126014aee7e2886ae3f2acb4e
bahaaelseisy-sudo <bahaaelseisy@gmail.com>
2026-01-31 17:35:45 +0300

    Add files via upload

--- compute.tf
+++ compute.tf
@@ -1,10 +1,10 @@
 resource "azurerm_network_interface" "DC_Nic" {
   name = "DC_Nic"
-  resource_group_name = azurerm_resource_group.hub_rg
+  resource_group_name = azurerm_resource_group.hub_rg.name
   location = azurerm_resource_group.hub_rg.location
   ip_configuration {
     name = "internal"
-    subnet_id = azurerm_subnet.Identity_subnet
+    subnet_id = azurerm_subnet.Identity_subnet.id
     private_ip_address_allocation = "Dynamic"
   }
   
