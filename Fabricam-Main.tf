05defdc0636036df55894f7e1b33249d3798d060
bahaaelseisy-sudo <bahaaelseisy@gmail.com>
2026-02-02 12:35:43 +0300

    Add files via upload

--- Fabricam-Main.tf
+++ Fabricam-Main.tf
@@ -79,5 +79,5 @@ resource "azurerm_subnet" "on-prem-resources" {
   resource_group_name = azurerm_resource_group.hub_rg.name
   virtual_network_name = azurerm_virtual_network.onprem-vnet.name
   name = "on-prem-resources"
-  
+  address_prefixes = ["10.3.0.0/24"]
 }
